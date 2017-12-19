0695234f1183c52cb429ee4f40c6d3ae4f54618b
meanmail <meanmail@mail.ru>
2017-12-19 13:35:13 +0700

    Update antlr python runtime

--- complexity/visitors/base_visitor.py
+++ complexity/visitors/base_visitor.py
@@ -33,6 +33,9 @@ class BaseVisitor(ParseTreeVisitor):
 
 class EmptyVisitor(object):
     def __init__(self):
+        self.a = 0
+        self.b = 0
+        self.c = 0
         self.success = False
 
     @property
